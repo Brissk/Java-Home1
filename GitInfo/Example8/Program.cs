b59645a8b50733c44a4639378ec06f91c760441b
«Mikle <cobra-mikle@mail.ru>
2022-04-14 22:31:36 +0300

    Чётный ряд чисел

---
+++ GitInfo/Example8/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 9;
+int i = 2;
+while (i <= a)
+{
+    if (i == a || i == a - 1)
+    {
+        Console.Write($"{i}.");
+    }
+    else{
+   Console.Write($"{i}, ");
+    }
+   i+=2;
+}
+
+
+
+
+
+
+
