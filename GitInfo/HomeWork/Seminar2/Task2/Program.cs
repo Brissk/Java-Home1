ec03c098a20c8b56d0a141d713e3002c98f082e9
«Mikle <cobra-mikle@mail.ru>
2022-04-20 21:19:23 +0300

    ()

--- GitInfo/HomeWork/Seminar2/Task2/Program.cs
+++ GitInfo/HomeWork/Seminar2/Task2/Program.cs
@@ -1,9 +1,15 @@
-﻿int x = new Random().Next(100,1000);
+﻿/* Задача 2: Напишите программу, которая выводит
+случайное трёхзначное число и удаляет вторую цифру
+этого числа.*/
+
+int x = new Random().Next(100,1000);
 
 System.Console.WriteLine(x);
 
 int y = x%10;
 int z = x/100;
+
+System.Console.WriteLine($"{z}{y}");
 
 
 
