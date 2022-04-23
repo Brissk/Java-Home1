5b74bbe1e4d3ec661496abda9b19b17ca2979952
«Mikle <cobra-mikle@mail.ru>
2022-04-23 20:35:04 +0300

    кубы

---
+++ GitInfo/HomeSeminar3/Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cubic(int x)
+{
+    int i = 1;
+    int square = 0;
+     while ( i <= x)
+     {
+         square = i*i*i;
+         i++;
+         System.Console.WriteLine(square);
+     }    
+
+}
+
+Cubic(5);
