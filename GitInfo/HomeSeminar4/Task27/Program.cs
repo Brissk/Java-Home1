1d5b88dc30e3e53b3b4eb1bcc6647eaa8140ad84
«Mikle <cobra-mikle@mail.ru>
2022-04-24 08:57:55 +0300

    Сложение

---
+++ GitInfo/HomeSeminar4/Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27  Напишите функцию, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Summary(int x)
+{
+    int result = 0;
+
+    for (int i = 0; x != 0 ;i++)
+    {
+        result += (x % 10);
+        x /= 10;
+    }
+
+    return result;
+
+}
+
+System.Console.WriteLine(Summary(452));
+System.Console.WriteLine(Summary(82));
+System.Console.WriteLine(Summary(9012));
