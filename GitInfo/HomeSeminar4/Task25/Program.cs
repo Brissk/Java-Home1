3c539d05f05b9d354bd604431cea658b84abdf20
«Mikle <cobra-mikle@mail.ru>
2022-04-24 08:32:01 +0300

    Степень

---
+++ GitInfo/HomeSeminar4/Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 25  Напишите (функцию) цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+double Grade(int A, int B)
+{
+    double result = Math.Pow(A,B);
+    
+    return result;
+    
+}
+
+System.Console.WriteLine(Grade(2,4));
+System.Console.WriteLine(Grade(3,5));
