d6c50e2701c411488def9d38901d45f0939fdb6a
«Mikle <cobra-mikle@mail.ru>
2022-04-30 15:00:16 +0300

    Positive numbers

---
+++ GitInfo/HomeSeminar6/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int PositiveNumber(string s)
+{
+    string [] stringArray = new string [s.Length];
+    stringArray = s.Split();
+    
+    int positive = 0;    
+    for (int i = 0; i < stringArray.Length; i++)
+    {        
+        if(int.Parse(stringArray[i]) > 0)
+        positive++;                       
+    }
+    System.Console.WriteLine("Количество положительных чисел: " + positive); 
+    return positive;
+}
+
+PositiveNumber("0 7 8 -2 -2");
