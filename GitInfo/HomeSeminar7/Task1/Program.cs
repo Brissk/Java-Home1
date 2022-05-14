236df4ce85061b75616eb5ec7bf4f54efec3d0be
«Mikle <cobra-mikle@mail.ru>
2022-05-14 14:21:03 +0300

    random nums

---
+++ GitInfo/HomeSeminar7/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void FillArrayRandom(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Math.Round(new Random().NextDouble()*10,1);
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+double[,] array = new double[3,4];
+FillArrayRandom(array);
+PrintArray(array);
