49ab1840a2cf3b2c23cc6c287e60a63cc8c7e189
«Mikle <cobra-mikle@mail.ru>
2022-05-16 23:46:40 +0300

    threedimentionmassive

---
+++ GitInfo/HomeSeminar8/Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+void FillArrayInARow(int[,,] array)
+{
+    int index = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+     {   for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = i + j + k + index;
+            }
+            index+=2;
+        }
+        index+=2;
+     }
+}
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.WriteLine($"[{i},{j},{k}] - " + array[i, j, k] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+
+int[,,] array = new int[3, 3, 3];
+FillArrayInARow(array);
+PrintArray(array);
+
