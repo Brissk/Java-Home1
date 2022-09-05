5136bc108a71f65fb9dc3a50cd94457883feea06
«Mikle <cobra-mikle@mail.ru>
2022-09-05 20:21:18 +0300

    Home1

---
+++ GitInfo/Java/Tusk0.java
@@ -0,0 +1,24 @@
+package Java;
+
+/* Найти наибольшую общую последовательность
+ */
+// import java.util.ArrayList;
+
+
+public class Tusk0 {
+    public static void main(String[] args) {
+        
+    }
+
+    int [] a = new int [] {1,2,3,4,5};
+    int [] b = new int [] {3,4,5,1,2};
+
+    static void func(int[]a){
+        
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                
+            }
+        }
+    }
+}
---
+++ GitInfo/Java/Tusk2_copy.java
@@ -0,0 +1,30 @@
+
+package Java;
+
+public class Tusk2_copy {
+
+    public static void main(String[] args) {
+        System.out.println(transform(2, 7, 1, 2));
+    }
+
+    public static int transform(int start, int end, int com1, int com2) {
+        int [] ways = new int [end+1];
+        ways[start] = 1;
+        for (int i = start+com1; i <= end; i++) {
+            if(i%com2==0){
+                ways[i] = ways[i-com1] + ways[i/com2];
+            }
+            else{
+                ways[i] = ways[i-com1];
+            }
+        }
+        return ways[end]; //Количество маршрутов из "start" в "end"
+    }
+
+}
+
+/* public static String transform1(int start, int end, int c, int d) {
+    if (start == end){
+
+    }
+} */
